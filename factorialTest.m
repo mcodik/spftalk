9b33f51fdaeec7ea3424e889b2218c99cd70f5e0
mcodik <mcodik@gmail.com>
2016-02-28 13:20:01 -0500

    Remove negative test case

--- factorialTest.m
+++ factorialTest.m
@@ -6,5 +6,3 @@ assert(1 == factorial(0), 'factorial(0) should be 1');
 %% Test number
 assert(3628800 == factorial(10), 'factorial(10) should be 3628800');
 
-%% Test negative number
-assert(1 == factorial(-10), 'factorial of a negative should be 1');
